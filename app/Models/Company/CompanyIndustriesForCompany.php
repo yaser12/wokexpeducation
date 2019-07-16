13bbf6e008436dfb799d2bbe83a79138de6429de
yaser <engineeryaseromran@gmail.com>
2019-07-16 07:03:12 +0300

    yaser omran company_specialties_for_company company_industries_for_company

---
+++ app/Models/Company/CompanyIndustriesForCompany.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Company;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyIndustriesForCompany extends Model
+{
+    protected $table = 'company_industries_for_company';
+}
---
+++ app/Models/Company/CompanySpecialtiesForCompany.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Company;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanySpecialtiesForCompany extends Model
+{
+    protected $table = 'company_specialties_for_company';
+}
